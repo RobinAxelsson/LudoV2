78d6e9812bc5ac2d2cf9c39f07c19a1e1eb1c7d4
RobinAxelsson <axelsson.robin@outlook.com>
2021-05-18 16:14:49 +0200

    player coords and game foreign key

--- Source/LudoAPI/Models/Player.cs
+++ Source/LudoAPI/Models/Player.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using LudoAPI.DataAccess;
 using LudoGame.GameEngine;
 
@@ -6,10 +7,12 @@ namespace LudoAPI.Models
 {
     public record Player
     {
+        [Key]
+        public int GameID { get; set; }
         public int AccountId { get; set; }
         public bool NextToThrow { get; set; }
         public int Result { get; set; }
-        public ICollection<Coordinate> Pawns { get; set; }
+        public ICollection<Coordinate> PawnCoords { get; set; }
         public GameEnum.TeamColor Color { get; set; }
         public ModelEnum.PlayerType Type { get; set; }
     }
