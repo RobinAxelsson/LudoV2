7a7b99fd2808aa9bc7ba702277c68f4316a3e01b
Albin <almalbin98@gmail.com>
2021-05-21 13:05:36 +0200

    Added InvitePlayer-endpoint

--- Source/LudoAPI/Controllers/GameController.cs
+++ Source/LudoAPI/Controllers/GameController.cs
@@ -94,6 +94,57 @@ namespace LudoAPI.Controllers
             _repository.SaveChanges();
             return Ok(game);
         }
+                [Authorize]
+        [HttpPost]
+        [Route("CreateGame")]
+        public IActionResult InvitePlayer(string gameId, string[] recipients)
+        {
+            var token = Request.Headers.FirstOrDefault(p => p.Key == "Authorization")
+                .Value.FirstOrDefault()
+                ?.Replace("Bearer ", "");
+            var account = _repository.AccountTokens.Include(a => a.Account)
+                .SingleOrDefault(t => t.Token == token)
+                ?.Account;
+            if (account == null) return NotFound($"Couldn't find account");
+            var game = _repository.Games.SingleOrDefault(g => g.GameId == gameId);
+            if (game == null) return NotFound($"Could not find a game with the ID: {gameId}");
+            //Make sure no more than 3 people has been invited
+            if (recipients.Length > 3) return UnprocessableEntity("You cannot invite more than 3 players");
+            //Check if recipients has been inputted.
+            if (recipients.Length == 0)
+                return Conflict("Recipients cannot be empty");
+            
+                //Create a list that will fill up with each address that wasn't found.
+            var notFoundAddresses = new List<string>();
+            recipients.ToList()
+                .ForEach(a =>
+                {
+                    if (!_repository.Accounts.Select(x => x.EmailAdress).ToList().Contains(a))
+                    {
+                        notFoundAddresses.Add(a);
+                    }
+                });
+            if (notFoundAddresses.Count > 0)
+            {
+                /*
+                We cannot input the list as a parameter in the message below because-
+                it will be typed to a .ToString() which results in just "System.Collection.List"
+                So we format a string instead
+                */
+                var message = "";
+                notFoundAddresses.ForEach(s => message += s + "\n");
+                return NotFound($"Below email address(es) has no registered user\n{message}");
+            }
+
+            //Create a placeholder account object that contains information from InviteBody.
+            //This is to avoid providing unnecessary data which would be the case if we would input an account object instead
+            var placeholder = new Account {PlayerName = account.PlayerName,};
+            var client = new EmailClient(_repository, _configuration);
+            //This will send invites to each recipient
+            client.SendInvite(recipients, game, placeholder);
+            return Ok(game);
+        }
+
 
         [Authorize]
         [HttpPost]
