aac0a38337c04dfb30a4428c8d9940829d007475
Albin <almalbin98@gmail.com>
2021-05-24 15:22:56 +0200

    Added Login design and banner

---
+++ Source/LudoWeb/Hubs/GameHub.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Concurrent;
+using LudoAPI.Models;
+using LudoGame.GameEngine.Classes;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.SignalR;
+
+namespace LudoWeb.Hubs
+{
+    public class GameHub : Hub
+    {
+        //List of active players on the server
+        private static readonly ConcurrentBag<Player> Players = new ConcurrentBag<Player>();
+        //List of active games on the server
+        private static readonly ConcurrentBag<Game> Games = new ConcurrentBag<Game>();
+        
+        
+        public void OnGameStart(string connectionId)
+        {
+            //Send data to API
+            //Get data back
+            //Send that data in SignalR
+          //  Clients.Client(Context.ConnectionId).SendAsync();
+        }
+
+        public void BroadcastMessage(string user, string message)
+        {
+            Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+       
+    }
+}
---
+++ Source/LudoWeb/Pages/Login.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LudoWeb.Pages
+{
+    public class login : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
