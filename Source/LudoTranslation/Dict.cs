235390e28590de4ad0c32d1f883bf97af683bd9e
Albin <almalbin98@gmail.com>
2021-05-24 23:17:46 +0200

    Updated language Added select-box for registration form Sorted Dict with regions

--- Source/LudoTranslation/Dict.cs
+++ Source/LudoTranslation/Dict.cs
@@ -3,17 +3,22 @@
 namespace LudoTranslation {
     public record Dict
     {
-        public string Welcome { get; private set; }
-        public string FirstName { get; private set; }
-        public string LastName { get; private set; }
+        #region Email
         public string Email_Subtitle { get; private set; }
         public string Email_Title { get; private set; }
         public string Email_Subject { get; private set; }
+        #endregion
+
+        #region LoginPage
         public string Login_BodyTitle { get; private set; }
         public string Login_AccountName { get; private set; }
         public string Login_Password { get; private set; }
         public string Login_LoginButton { get; private set; }
         public string Login_PageHeaderTitle { get; private set; }
+
+        #endregion
+     
+        #region RegisterPage
         public string Register_BodyTitle { get; private set; }
         public string Register_AccountName { get; private set; }
         public string Register_Password { get; private set; }
@@ -25,5 +30,8 @@ namespace LudoTranslation {
         public string Register_PasswordPlaceholderText { get; private set; }
         public string Register_RetypePasswordPlaceholderText { get; private set; }
         public string Register_AccountNamePlaceholderText { get; private set; }
+        public string Register_ChooseLanguage { get; private set; }
+        #endregion
+        
     }
 }
